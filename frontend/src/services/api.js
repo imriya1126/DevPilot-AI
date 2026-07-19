4ab20bfa696b3a13befd547d1a3d814d020b0e2b
imriya1126 <riyakakkar0501@gmail.com>
2026-07-19 23:36:54 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://devpilot-ai-backend-6d8m.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
