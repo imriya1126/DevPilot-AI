d31e2ec67733cdf5c8a09bdf1a19ffd976bf0087
imriya1126 <riyakakkar0501@gmail.com>
2026-07-20 00:27:02 +0530

    Fix API import path

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import API from "../api";
+import API from "../services/api";
 import "../styles/Login.css";
 
 function Register() {
